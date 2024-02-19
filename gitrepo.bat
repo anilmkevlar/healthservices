d3454560591f33e24e1ed915ccbf8e29c5c33094
jaskaran <anil@kevlarsystems.com>
2024-02-19 13:46:50 +0530

    update docker compose

--- gitrepo.bat
+++ gitrepo.bat
@@ -4,9 +4,8 @@ git clone https://github.com/microsoft/dicom-server.git ./dicom-server
 git clone https://github.com/Anilm19/fhirLoader-automate.git ./fhirLoader-automate
 
 git config --global core.autocrlf false
-git clone https://github.com/anilmkevlar/fhirloader.git ./fhirLoader-automate/synthea
+git clone https://github.com/synthetichealth/synthea.git ./fhirLoader-automate/synthea
 git config --global core.autocrlf true
-
 
 
 cd fhir-server
@@ -23,8 +22,12 @@ git pull
 
 
 cd ..
+cd ..
 
-docker volume create SqlDb
+xcopy /y .\synthea.properties .\fhirLoader-automate\synthea\src\main\resources\synthea.properties
+
+docker volume create dicomSqlDb
+docker volume create fhirSqlDb
 
 docker compose up
 pause
